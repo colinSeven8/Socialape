289a6d64ad8ae25ee72645beba26003aafe5bece
colinSeven8 <ccnorse80@gmail.com>
2020-09-24 08:35:00 -0600

    Added the firebase authorization middleware (FBAuth) to the 'scream post route

--- functions/index.js
+++ functions/index.js
@@ -38,10 +38,38 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));
 });
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1]; //Bearer is convention and precedes the actual token
+    } else {
+        console.log('No token found');
+        return res.status(403).json({ error: 'Unauthorized '});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        req.user = decodedToken;
+        console.log(decodedToken);
+        return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1) //Limit to one record
+        .get();
+    })
+    .then(data => {
+        req.user.handle = data.docs[0].data().handle;
+        return next(); //Allows request to proceed
+    })
+    .catch(err => {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json(err);
+    });
+}
+
+app.post('/scream', FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
     db
@@ -144,7 +172,9 @@ app.post('/login', (req, res) => {
 
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
-    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
     .then(data => {
         return data.user.getIdToken();
     })
