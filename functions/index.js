80f0196702bed6f63ca5765b0f81294199a294c9
colinSeven8 <ccnorse80@gmail.com>
2020-11-12 15:02:48 -0700

    Added the delete route to delete a comment based on it's id

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,40 @@
-const functions = require('firebase-functions');
-const app = require('express')();
+const functions = require("firebase-functions");
+const app = require("express")();
 
-const FBAuth = require('./util/fbAuth');
+const FBAuth = require("./util/fbAuth");
 
-const { 
-    getAllScreams, 
-    postOneScream, 
-    getScream, 
-    commentOnScream,
-    likeScream,
-    unlikeScream
- } = require('./handlers/screams');
-const { 
-    signup, 
-    login, 
-    uploadImage, 
-    addUserDetails, 
-    getAuthenticatedUser 
-} = require('./handlers/users');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
+} = require("./handlers/screams");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require("./handlers/users");
 
 //Scream routes
-app.get('/screams', getAllScreams);
-app.post('/scream', FBAuth, postOneScream);
-app.get('/scream/:screamId', getScream);
-app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
-app.get('/scream/:screamId/like', FBAuth, likeScream);
-app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
-//Delete a scream
+app.get("/screams", getAllScreams);
+app.post("/scream", FBAuth, postOneScream);
+app.get("/scream/:screamId", getScream);
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 
 //Users routes
-app.post('/signup', signup);
-app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 // exports.api = functions.https.onRequest(app);
-exports.api = functions.region('us-west3').https.onRequest(app); //region for SLC
+exports.api = functions.region("us-west3").https.onRequest(app); //region for SLC
