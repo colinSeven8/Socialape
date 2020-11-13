05b2926fdf820114cbd906fd17096ed5a897bb92
colinSeven8 <ccnorse80@gmail.com>
2020-11-13 13:12:32 -0700

    Started adding functionality for FB triggers that will notify the user when something is commented on, liked or unliked

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,55 +1,65 @@
 let db = {
-    users: [
-        {
-        userId: 'fdsgrvrsvsdvdsfv',
-        email: 'user@email.com',
-        handle: 'user',
-        createdAt: '2020-10-15T10:59:52.798Z',
-        imageUrl: 'image/asgasvsvfdf/dfdfvfvf',
-        bio: 'Hello, my name is user, nice to meet you',
-        website: 'https://user.com',
-        location: 'Salt Lake City, UT'
-        }
-    ],
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream body',
-            createdAt: '2020-09-18T17:25:20.061Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ],
-    comments: [
-        {
-            userHandle: 'user',
-            screamId: 'fbeaergerdgteg',
-            body: 'Nice one, mate!',
-            createdAt: '2020-09-18T17:25:20.061Z'
-        }
-    ]
+  users: [
+    {
+      userId: "fdsgrvrsvsdvdsfv",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2020-10-15T10:59:52.798Z",
+      imageUrl: "image/asgasvsvfdf/dfdfvfvf",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "Salt Lake City, UT",
+    },
+  ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-09-18T17:25:20.061Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+  comments: [
+    {
+      userHandle: "user",
+      screamId: "fbeaergerdgteg",
+      body: "Nice one, mate!",
+      createdAt: "2020-09-18T17:25:20.061Z",
+    },
+  ],
+  notifications: [
+    {
+      recipient: "user",
+      sender: "john",
+      read: "true | false",
+      screamId: "dfbdfbaerdfvdfvbf",
+      type: "like | comment",
+      createdAt: "2020-09-18T17:25:20.061Z",
+    },
+  ],
 };
 const userDetails = {
-    // Redux data
-    credentials: {
-        userId: 'HH8GO384NO8VLE9349FJ49',
-        email: 'user@email.com',
-        handle: 'user',
-        createdAt: '2020-10-28T10:59:52.798Z',
-        imageUrl:'image/dfbdfbaebdbf/dfbdfbdfbdb',
-        bio: 'Hello, my name is user, nice to meet you',
-        website: 'https://user.com',
-        location: 'London, UK'
+  // Redux data
+  credentials: {
+    userId: "HH8GO384NO8VLE9349FJ49",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2020-10-28T10:59:52.798Z",
+    imageUrl: "image/dfbdfbaebdbf/dfbdfbdfbdb",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "London, UK",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "ijorinovielierij",
     },
-    likes: [
-        {
-            userHandle: 'user',
-            screamId: 'ijorinovielierij'
-        },
-        {
-            userHandle: 'user',
-            screamId: 'oijg9w8rhouinivd'
-        }
-    ]
-}
+    {
+      userHandle: "user",
+      screamId: "oijg9w8rhouinivd",
+    },
+  ],
+};
 //Doing this will minimize the number of "reads" on my FIrebase DB so it doesn't rape me on charges $
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-
 const FBAuth = require("./util/fbAuth");
+
+const { db } = require("./util/admin");
 
 const {
   getAllScreams,
@@ -38,3 +39,75 @@ app.get("/user", FBAuth, getAuthenticatedUser);
 
 // exports.api = functions.https.onRequest(app);
 exports.api = functions.region("us-west3").https.onRequest(app); //region for SLC
+
+// Create a notification when someone likes something
+exports.createNotificationsOnLike = functions
+  .region("us-west3")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return; //Only holds a write result
+      })
+      .catch((err) => {
+        console.error(err);
+        return; //Don't need to respond because this is a DB trigger
+      });
+  });
+
+// Create a notification when someone unlikes something
+exports.deleteNotificationOnUnLike = functions
+  .region("us-west3")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`) //The ID of the like is the same as the ID of the notification
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+// Create a notification when someone comments
+exports.createNotificationsOnComment = functions
+  .region("us-west3")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return; //Only holds a write result
+      })
+      .catch((err) => {
+        console.error(err);
+        return; //Don't need to respond because this is a DB trigger
+      });
+  });
