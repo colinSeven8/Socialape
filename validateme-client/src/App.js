026ddedca13f0ac58808f5dd093c01b8f8cec1e6
colinSeven8 <ccnorse80@gmail.com>
2021-01-22 17:15:41 -0700

    Started adding front end, React skeleton routes

---
+++ validateme-client/src/App.js
@@ -0,0 +1,24 @@
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
