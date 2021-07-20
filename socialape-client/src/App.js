f44a13e11ba5a21aeb357d811bd51f6ee08f548b
colinSeven8 <ccnorse80@gmail.com>
2021-07-20 16:19:13 -0600

    Saving before I remove some follders

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -2,6 +2,8 @@ import { AppBar } from "@material-ui/core";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //Components
 import NavBar from "./components/Navbar";
 //Pages
@@ -9,21 +11,43 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#484e92",
+      main: "#142664",
+      dark: "#000039",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#fffff0",
+      main: "#f0d6bd",
+      dark: "#bda58d",
+      contrastText: "#000",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
