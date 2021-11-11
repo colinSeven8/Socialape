73760e2226eec92db106578319ef1ebe3b435c93
colinSeven8 <ccnorse80@gmail.com>
2021-11-11 15:54:41 -0700

    Added Navbar but Firebase is not deploying properly

--- validateme-client/src/App.js
+++ validateme-client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { MuiThemeProvider as ThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
 
 //Components
 import Navbar from "./components/Navbar";
@@ -10,21 +12,43 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#cfd8dc",
+      main: "#607d8b",
+      dark: "#263238",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#f5f5f5",
+      main: "#9e9e9e",
+      dark: "#212121",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-          </div>
-        </Router>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </div>
+          </Router>
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- validateme-client/src/pages/home.js
+++ validateme-client/src/pages/home.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
-const home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-);
+class home extends Component {
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <p>{scream.body}</p>)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
+}
 
 export default home;
